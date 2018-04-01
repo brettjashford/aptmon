a3184c1f36ee0bd75a0c6959f15589dbc2fa0604
Brett Ashford <brettjashford@gmail.com>
2018-04-01 15:44:06 -0400

    hue integration to get fetch sensors and get temperatures

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+require('dotenv').config()
 const {CronJob} = require('cron');
+const Hue = require('./lib/hue');
+const {info, success, inspect} = require('./lib/log');
 
+(async () => {
+    const hue = new Hue();
+    await hue.init();
+    const temps = await hue.getTemps();
+    inspect(temps);
 
-const job = new CronJob({
-    cronTime: '* * * * * *',
-    start: false,
-    timeZone: 'America/New_York',
-    onTick() {
-        
-    }
-});
+    // const job = new CronJob({
+    //     cronTime: '* * * * * *',
+    //     start: false,
+    //     timeZone: 'America/New_York',
+    //     onTick() {
+    //
+    //     }
+    // });
+    // job.start();
 
-job.start();
+})();
